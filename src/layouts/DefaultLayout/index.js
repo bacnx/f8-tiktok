98bbd0297bf2afe2df8fc75fbd5c17ac62144694
Bac Nguyen <nxbac.dev@gmail.com>
2022-09-20 12:13:27 +0700

    Build default layout frame

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,12 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Header from '../components/Header';
 import SideBar from '../components/SideBar';
 
+const cx = classNames.bind(styles);
+
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <SideBar />
-      <div className="content">{children}</div>
+      <div className={cx('container')}>
+        <SideBar />
+        <div className={cx('content')}>{children}</div>
+      </div>
     </div>
   );
 }
