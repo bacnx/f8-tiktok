a8b1095645734122ad63ed72e5c62d3ed8249a02
Bac Nguyen <nxbac.dev@gmail.com>
2023-01-05 05:07:34 +0700

    Login button at SideBar

--- src/layouts/components/SideBar/SideBar.js
+++ src/layouts/components/SideBar/SideBar.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
-import configs from '~/configs';
 
+import configs from '~/configs';
 import styles from './SideBar.module.scss';
 import {
   HomeActiveIcon,
@@ -12,10 +12,17 @@ import {
 } from '~/components/Icons';
 import Menu, { MenuItem } from './Menu';
 import SuggestedAccounts from '~/components/SuggestedAccounts';
+import Button from '~/components/Button';
+import { useModal } from '~/hooks';
+import Login from '~/components/Login';
+import auth from '~/auth';
 
 const cx = classNames.bind(styles);
 
 function SideBar() {
+  const { isShowing, toggle } = useModal();
+  const currentUser = auth.getCurrentUser();
+
   return (
     <aside className={cx('wrapper')}>
       <Menu className={cx('menu')}>
@@ -28,6 +35,20 @@ function SideBar() {
         />
         <MenuItem title="LIVE" to={configs.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
       </Menu>
+
+      {!currentUser && (
+        <>
+          <span className={cx('line')}></span>
+          <div className={cx('login')}>
+            <p className={cx('login-desc')}>Log in to follow creators, like videos, and view comments.</p>
+            <Button className={cx('login-button')} size="large" type="border" color="primary" block onClick={toggle}>
+              Login
+            </Button>
+          </div>
+
+          <Login isShowing={isShowing} hide={toggle} />
+        </>
+      )}
 
       <span className={cx('line')}></span>
       <SuggestedAccounts label="Suggested accounts" />
